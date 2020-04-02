425afde939184300aa7725e2d11f8e9bf07efbf8
ChanJeff123 <chenwanyuan813@gmail.com>
2020-04-02 21:45:38 +0800

    add 0点

---
+++ ts时间戳.py
@@ -0,0 +1,18 @@
+import time
+
+ctime10=str(int(round(time.time())))
+ctime13=str(int(round(time.time() * 1000)))
+ctime16=str(int(round(time.time() * 1000000)))
+ctime18=str(int(round(time.time() * 100000000)))
+
+# 13位 当天0点
+t = time.localtime(time.time())
+time1 = time.mktime(time.strptime(time.strftime('%Y-%m-%d 00:00:00', t),'%Y-%m-%d %H:%M:%S'))
+et=int(time1)*1000
+st=et-86400000
+print(st,et)
+
+print(ctime10)
+print(ctime13)
+print(ctime16)
+print(ctime18)
