3e35cf5734a3c582e5fa769cccf907b30ab41976
ChanJeff123 <404487132@qq.com>
2020-04-07 17:57:20 +0800

    update 今日昨日 当前时间

--- ts时间戳.py
+++ ts时间戳.py
@@ -1,5 +1,5 @@
 import time
-
+import datetime
 ctime10=str(int(round(time.time())))
 ctime13=str(int(round(time.time() * 1000)))
 ctime16=str(int(round(time.time() * 1000000)))
@@ -16,3 +16,14 @@ print(ctime10)
 print(ctime13)
 print(ctime16)
 print(ctime18)
+
+now1=datetime.datetime.now()
+hour_now=now1.strftime('%H')
+today = datetime.date.today()
+oneday=datetime.timedelta(days=1) 
+yesterday=today-oneday
+# table="huya_{}".format(yesterday).replace("-","")
+print(now1)
+print(hour_now)
+print(today)
+print(yesterday)
