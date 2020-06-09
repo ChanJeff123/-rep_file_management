338fa7daaf31e1700ec64413f255a1494ac7b257
ChanJeff123 <404487132@qq.com>
2020-06-09 22:25:41 +0800

    数据下载

--- picDownload图片下载脚本.py
+++ picDownload图片下载脚本.py
@@ -1,5 +1,6 @@
 import requests
 import os
+import pymysql
 headers = {
     'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.106 Safari/537.36',
@@ -22,9 +23,25 @@ def ensure_dir(file_path):
         print("新建文件夹'%s'成功"%file_path)
     else:
         print("'%s'已经存在"%file_path)
+
 if __name__ == "__main__":
+
     f="./jpg/"
     ensure_dir(f)
-    url="https://sta-op.douyucdn.cn/dycatr/a184836e114db124347b36b702a17638.png"
-    name="cs.jpg"
-    downloadp(url,name,f)
+    conn = pymysql.connect(
+        user = 'root',
+        host = 'localhost',
+        password= '',
+        db = 'guwan',
+        port = 3306,
+    )
+    cur = conn.cursor()
+    sqlstr="select * from game_pic where id ='%s'"
+    for id in range(1,1123):
+        cur.execute(sqlstr%id)
+        a=cur.fetchone()
+        name=a[1]+".png"
+        url=a[2]
+        downloadp(url,name,f)
+    cur.close()
+    conn.close()
